fc5c6fd1439d4fc1c33102edab955d175b02e83b
anthinkingcoder <jojo837769723@gmail.com>
2019-04-01 17:25:38 +0800

    test:测试节点移动到右边，beforeNode不为空的情况

--- test/diff.test.js
+++ test/diff.test.js
@@ -16,23 +16,37 @@ const diff = require('../lib/diff')
 // ], 'root')
 //
 // let patches = diff(virtualDom, virtualDom2)
+//
 
+
+// let virtualDom = createElement('ul', { class: 'list' }, [
+//   createElement('li', { class: 'item' }, 'A'),
+//   createElement('li', { class: 'item' }, 'B'),
+// ], 'root');
+//
+// let virtualDom2 =createElement('ul', { class: 'list' }, [
+//   createElement('li', { class: 'item' }, 'E'),
+//   createElement('li', { class: 'item' }, 'D'),
+//   createElement('li', { class: 'item' }, 'A'),
+//   createElement('li', { class: 'item' }, 'C'),
+// ], 'root')
+
+// let patches = diff(virtualDom, virtualDom2)
 
 
 let virtualDom = createElement('ul', { class: 'list' }, [
   createElement('li', { class: 'item' }, 'A'),
   createElement('li', { class: 'item' }, 'B'),
+  createElement('li', { class: 'item' }, 'C'),
+  createElement('li', { class: 'item' }, 'D'),
 ], 'root');
 
 let virtualDom2 =createElement('ul', { class: 'list' }, [
   createElement('li', { class: 'item' }, 'E'),
-  createElement('li', { class: 'item' }, 'D'),
   createElement('li', { class: 'item' }, 'A'),
   createElement('li', { class: 'item' }, 'C'),
+  createElement('li', { class: 'item' }, 'D'),
 ], 'root')
 
 let patches = diff(virtualDom, virtualDom2)
-
-Object.keys(patches).forEach((key) => {
-  console.log(`${key}:${JSON.stringify(patches[key])}`)
-})
+console.log(JSON.stringify(patches))
